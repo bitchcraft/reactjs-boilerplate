b20f3ba8566289efc1fff5e8e05c09def00a17d5
thiemok <krausethiemo@googlemail.com>
2018-07-14 18:20:12 +0200

    feat(npx support): add project folder creation

--- tools/jumpstartProject.js
+++ tools/jumpstartProject.js
@@ -8,9 +8,19 @@
  */
 
 const path = require('path');
+const mkdirp = require('mkdirp');
+
 const argv = process.argv.slice(2);
 const cwd = process.cwd();
 
+function createProjectDirectory(targetPath) {
+	if (!path.isAbsolute(targetPath)) {
+		targetPath = path.join(cwd, targetPath);
+	}
+	mkdirp.sync(targetPath);
+}
+
 (function cmd() {
-	console.log(cwd);
+	/** create new project folder */
+	createProjectDirectory(argv[0]);
 })();
