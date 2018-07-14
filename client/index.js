e786bdaef4f9f020686fc35e7fc304d8c3873128
thiemok <krausethiemo@googlemail.com>
2018-07-14 13:49:35 +0200

    feat(PEP): #22 replace react-touch-tap-plugin with PEP

--- client/index.js
+++ client/index.js
@@ -1,13 +1,10 @@
 import 'babel-polyfill';
 import 'isomorphic-fetch';
+import 'pepjs-improved';
 import React from 'react';
-
-const injectTapEventPlugin = require('react-tap-event-plugin');
-
-injectTapEventPlugin();
+import app from './app';
 
 // Needed for React Developer Tools
 if (process.env.NODE_ENV === 'development') global.React = React;
 
-// needs to be a require, so injectTapEventPlugin works. wtf?!
-require('./app');
+app();
