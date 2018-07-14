249f9979d1c9d196f838abcfc16590ea95e302c9
thiemok <krausethiemo@googlemail.com>
2018-07-14 19:28:54 +0200

    feat(npx support): add praram validation and check for binary dependencies

--- tools/jumpstartProject.js
+++ tools/jumpstartProject.js
@@ -9,9 +9,48 @@
 
 const path = require('path');
 const mkdirp = require('mkdirp');
+const shell = require('shelljs');
 
 const argv = process.argv.slice(2);
 const cwd = process.cwd();
+const requiredBinaries = [ 'git' ];
+const usageMessage = 'USAGE: reactjs-boilerplate target';
+
+function checkRequiredArguments() {
+	let argumentsPresent = true;
+
+	if (typeof argv[0] !== 'string' || argv[0].length === 0) {
+		argumentsPresent = false;
+	}
+
+
+	if (!argumentsPresent) {
+		shell.echo('Missing arguments');
+		shell.echo(usageMessage);
+	}
+	return argumentsPresent;
+}
+
+function listMissingBinaries(binaries) {
+	const missingBinaries = binaries.filter(function(binary) {
+		return shell.which(binary) === null;
+	});
+
+	return missingBinaries;
+}
+
+function checkRequiredBinaries() {
+	const missingBinaries = listMissingBinaries(requiredBinaries);
+	if (missingBinaries.length) {
+		shell.echo(`ERROR: Required binaries missing. Please install ${
+			missingBinaries.slice(1).reduce(
+				function(acc, bin) { return `${acc}, ${bin}`; },
+				`${missingBinaries[0]}`)
+		}`);
+		return false;
+	}
+	return true;
+}
 
 function createProjectDirectory(targetPath) {
 	if (!path.isAbsolute(targetPath)) {
@@ -21,6 +60,10 @@ function createProjectDirectory(targetPath) {
 }
 
 (function cmd() {
+	/** check if required arguments are present and valid */
+	if (!checkRequiredArguments()) process.exit(1);
+	/** check if necessary binaries are present */
+	if (!checkRequiredBinaries()) process.exit(1);
 	/** create new project folder */
 	createProjectDirectory(argv[0]);
-})();
+}());
