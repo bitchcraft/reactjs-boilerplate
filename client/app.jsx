edb8877f9a9ff54d4cc21b294340190a71939562
thiemok <krausethiemo@googlemail.com>
2019-06-06 22:49:50 +0200

    fix(hrm): fix hot reloading

--- client/app.jsx
+++ client/app.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
+import { hot } from 'react-hot-loader/root';
 import { render } from 'react-dom';
 import MuiTheme from 'containers/MuiTheme';
 
 import Store from 'containers/store';
 
-const App = () => (
+const App = hot(() => (
 	<MuiTheme>
 		<Store />
 	</MuiTheme>
-);
+));
 
 export default () => render(<App />, document.getElementById('app-container'));
